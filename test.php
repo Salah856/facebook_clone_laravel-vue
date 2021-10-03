0fcaf2283cd27499efc4a59de25711c3d3dcadc4
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-03 15:57:39 +0200

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-use App\Greeting;
-Route::get('create-greeting', function () {
-$greeting = new Greeting;
-$greeting->body = 'Hello, World!';
-$greeting->save();
-});
-Route::get('first-greeting', function () {
-return Greeting::first()->body;
-});
